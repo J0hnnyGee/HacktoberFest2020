45c7b2ef4d65e67894682240a2ed028af0e77105
J0hnnyGee <61202910+J0hnnyGee@users.noreply.github.com>
2020-10-17 22:14:15 +0200

    Update Check_even_Odd.c

--- C/Check_even_Odd.c
+++ C/Check_even_Odd.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{   int x;
+int main(){
+    int x;
     printf("Enter a number \n");
      scanf("%d",&x);
      if(x%2==0)
-        printf("Number you entered is even");
+        printf("You entered %d, this number is even\n", x);
      else
-        printf("Number you entered is odd");
+        printf("You entered %d, this number is odd\n", x);
 
     return 0;
 }
